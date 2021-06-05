d34808279408174cc4cd38d3e46b200da3467b47
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-06-05 01:34:05 -0400

    More fields. Load from file specified in argv.

--- fibermc.py
+++ fibermc.py
@@ -1,5 +1,7 @@
 
 import requests
+import json
+from sys import argv
 
 import os
 from dotenv import load_dotenv
@@ -7,19 +9,12 @@ from dotenv import load_dotenv
 load_dotenv()
 WEBHOOK_URL_FIBERMC = os.getenv("WEBHOOK_URL_FIBERMC")
 
-
-message_content = """
-This is a test of a custom Discord webhook! -JP
-"""
-
 post_headers = {
     "content-type": "application/json",
 }
 
-post_data = {
-    "content": message_content,
-}
-
+with open(argv[1]) as f:
+    post_data = json.load(f)
 
 res = requests.post(WEBHOOK_URL_FIBERMC, json=post_data, headers=post_headers)
 
